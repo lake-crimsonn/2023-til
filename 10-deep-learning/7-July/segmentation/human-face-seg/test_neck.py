f2ea1052913d987a03da04ade472b1e5b65e72ee
lake-crimsonn <lakecrimsonn@gmail.com>
2023-07-30 22:54:55 +0900

    23073-

---
+++ 10-deep-learning/7-July/segmentation/human-face-seg/test_neck.py
@@ -0,0 +1,160 @@
+
+from train import create_dir, load_dataset
+from sklearn.metrics import f1_score, jaccard_score
+import tensorflow as tf
+from tqdm import tqdm
+from glob import glob
+import pandas as pd
+import cv2
+import numpy as np
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # 경고창 지우기
+
+
+global image_h
+global image_w
+global num_classes
+global classes
+global rgb_codes
+global cnt
+cnt = 0
+
+
+def grayscale_to_rgb(mask, rgb_codes):
+    h, w = mask.shape[0], mask.shape[1]
+    mask = mask.astype(np.int32)  # rgb코드에 넣기위해서
+    output = []
+
+    for i, pixel in enumerate(mask.flatten()):
+        output.append(rgb_codes[pixel])
+
+    output = np.reshape(output, (h, w, 3))
+    return output
+
+
+def save_results(image_x, mask, pred, save_image_path):
+    mask = np.expand_dims(mask, axis=-1)
+    mask = grayscale_to_rgb(mask, rgb_codes)
+
+    pred = np.expand_dims(pred, axis=-1)
+    pred = grayscale_to_rgb(pred, rgb_codes)
+
+    line = np.ones((image_x.shape[0], 10, 3)) * 255
+
+    # cat_images = np.concatenate([image_x, line, mask, line, pred], axis=1)
+    print(save_image_path)
+    cv2.imwrite(save_image_path, pred)
+
+
+if __name__ == "__main__":
+
+    """ Directory for storing files """
+    create_dir("C:\data\lapa\LaPa\\results")
+
+    """ Hyperparameters """
+    image_h = 512
+    image_w = 512
+    num_classes = 2
+
+    """ Paths """
+    dataset_path = "C:\data\lapa\LaPa"
+    model_path = os.path.join("C:\\data\\lapa\\LaPa\\files", "model.h5")
+
+    """ RGB Code and Classes """
+    rgb_codes = [
+        [0, 0, 0], [255, 255, 255]
+    ]
+
+    classes = [
+        "background", "neck"
+    ]
+
+    """ Loading the dataset """
+    (train_x, train_y), (valid_x, valid_y), (test_x,
+                                             test_y) = load_dataset(dataset_path)
+    print(f"Train: {len(train_x)}/{len(train_y)} - Valid: {len(valid_x)}/{len(valid_y)} - Test: {len(test_x)}/{len(test_x)}")
+    print("")
+
+    """ Load the model """
+    model = tf.keras.models.load_model(
+        model_path, compile=False)  # complie=True 디폴트, False로 설정 시 모델 구성을 수정할 수 있다. 그러면 model.compile()로 다시 컴파일 해야 한다.
+
+    """ Prediction & Evaluation """
+    SCORE = []
+    for x, y in tqdm(zip(test_x[:10], test_y[:10]), total=len(test_x[:10])):
+        """ Extract the name """
+        name = x.split("/")[-1].split(".")[0]
+
+        """ Reading the image """
+        image = cv2.imread(x, cv2.IMREAD_COLOR)
+        image = cv2.resize(image, (image_w, image_h))
+        image_x = image
+        image = image/255.0  # (H, W, 3)
+        image = np.expand_dims(image, axis=0)  # [1, H, W, 3]
+        image = image.astype(np.float32)
+
+        """ Reading the mask """
+        mask = cv2.imread(y, cv2.IMREAD_GRAYSCALE)
+        mask = cv2.resize(mask, (image_w, image_h))
+        mask = mask.astype(np.int32)
+
+        """ Prediction """
+        pred = model.predict(image, verbose=0)[0]
+        pred = np.argmax(pred, axis=-1)  # [0.1, 0.2, 0.1, 0.6] -> 3
+        pred = pred.astype(np.int32)
+
+        # cv2.imwrite("pred.png", pred * (255/11))
+
+        rgb_mask = grayscale_to_rgb(pred, rgb_codes)
+        # cv2.imwrite("C:\\data\\lapa\\LaPa\\results\\pred.png", rgb_mask)
+
+        """ Save the results """
+        cnt += 1
+        save_image_path = f"C:\\data\\lapa\\LaPa\\results\\{cnt}.png"
+        # print(save_image_path)
+        save_results(image_x, mask, pred, save_image_path)
+
+        """ Flatten the array """
+        mask = mask.flatten()
+        pred = pred.flatten()
+
+        labels = [i for i in range(num_classes)]
+
+        """ Calculating the metrics values """
+        f1_value = f1_score(mask, pred, labels=labels,
+                            average=None, zero_division=0)
+        jac_value = jaccard_score(
+            mask, pred, labels=labels, average=None, zero_division=0)
+
+        SCORE.append([f1_value, jac_value])
+
+    score = np.array(SCORE)
+    score = np.mean(score, axis=0)
+
+    f = open("C:\data\lapa\LaPa\files\score.csv", "w")
+    f.write("Class,F1,Jaccard\n")
+
+    l = ["Class", "F1", "Jaccard"]
+    print(f"{l[0]:15s} {l[1]:10s} {l[2]:10s}")
+    print("-"*35)
+
+    for i in range(num_classes):
+        class_name = classes[i]
+        f1 = score[0, i]
+        jac = score[1, i]
+        dstr = f"{class_name:15s}: {f1:1.5f} - {jac:1.5f}"
+        print(dstr)
+        f.write(f"{class_name:15s},{f1:1.5f},{jac:1.5f}\n")
+
+    print("-"*35)
+    class_mean = np.mean(score, axis=-1)
+    class_name = "Mean"
+
+    f1 = class_mean[0]
+    jac = class_mean[1]
+
+    dstr = f"{class_name:15s}: {f1:1.5f} - {jac:1.5f}"
+    print(dstr)
+    f.write(f"{class_name:15s},{f1:1.5f},{jac:1.5f}\n")
+
+    f.close()
--- 10-deep-learning/7-July/segmentation/human-face-seg/unet.py
+++ 10-deep-learning/7-July/segmentation/human-face-seg/unet.py
@@ -1,12 +1,12 @@
 
-import os # 출력 경고창 없애기
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" 
-
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
 from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
+import os  # 출력 경고창 없애기
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 
 def conv_block(inputs, num_filters):
-    x = Conv2D(num_filters, 3, padding="same")(inputs) 
+    x = Conv2D(num_filters, 3, padding="same")(inputs)
     x = BatchNormalization()(x)
     x = Activation("relu")(x)
 
@@ -16,40 +16,45 @@ def conv_block(inputs, num_filters):
 
     return x
 
+
 def encoder_block(inputs, num_filters):
     x = conv_block(inputs, num_filters)
     p = MaxPool2D((2, 2))(x)
     return x, p
 
+
 def decoder_block(inputs, skip, num_filters):
     # 일반적인 컨볼루션의 반대 방향으로 진행 up-conv
-    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(inputs) # 스트라이드가 2라서 크기가 2배로 증가 32->64
-    x = Concatenate()([x, skip]) # [64x64 512, 64x64 512] -> 64x64 1024
-    x = conv_block(x, num_filters) # 64x64, 1024 -> 64x64, 512
+    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(
+        inputs)  # 스트라이드가 2라서 크기가 2배로 증가 32->64
+    x = Concatenate()([x, skip])  # [64x64 512, 64x64 512] -> 64x64 1024
+    x = conv_block(x, num_filters)  # 64x64, 1024 -> 64x64, 512
     return x
 
+
 def build_unet(input_shape, num_classes):
-    inputs = Input(input_shape) # 512x512 3를 받아서 케라스텐서로 변경해줌
-    
-    s1, p1 = encoder_block(inputs, 64) # 512, 256 
-    s2, p2 = encoder_block(p1, 128) # 256, 128
-    s3, p3 = encoder_block(p2, 256) # 128,64
-    s4, p4 = encoder_block(p3, 512) # 64, 32
+    inputs = Input(input_shape)  # 512x512 3를 받아서 케라스텐서로 변경해줌
+
+    s1, p1 = encoder_block(inputs, 64)  # 512, 256
+    s2, p2 = encoder_block(p1, 128)  # 256, 128
+    s3, p3 = encoder_block(p2, 256)  # 128,64
+    s4, p4 = encoder_block(p3, 512)  # 64, 32
 
     # 플래튼 시키면 위치정보를 잃어버리니까 컨블럭에 입력
-    b1 = conv_block(p4, 1024) # 32x32 1024
+    b1 = conv_block(p4, 1024)  # 32x32 1024
 
     # 이전형태, 저장해둔 위치정보, 채널수
-    d1 = decoder_block(b1, s4, 512) # 64x64 512 
-    d2 = decoder_block(d1, s3, 256) # 128x128 256
-    d3 = decoder_block(d2, s2, 128) # 256x256 128
-    d4 = decoder_block(d3, s1, 64) # 512x512 64
+    d1 = decoder_block(b1, s4, 512)  # 64x64 512
+    d2 = decoder_block(d1, s3, 256)  # 128x128 256
+    d3 = decoder_block(d2, s2, 128)  # 256x256 128
+    d4 = decoder_block(d3, s1, 64)  # 512x512 64
 
     # 마지막은 클래스 수, 커널은 1, 멀티클래스니까 softmax
     outputs = Conv2D(num_classes, 1, padding="same", activation="softmax")(d4)
 
-    model = Model(inputs, outputs) # 모델 인스턴스 생성
+    model = Model(inputs, outputs)  # 모델 인스턴스 생성
     return model
+
 
 # 인터프리터에서 직접 실행이 되는 경
 if __name__ == "__main__":
